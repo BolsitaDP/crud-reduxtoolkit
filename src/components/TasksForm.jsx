4cb0e3a6495558c3b377649e09dd7d554d231f77
Santiago Giraldo <sgiraldo118@egmail.com>
2022-08-01 22:20:21 -0500

    CRUD RT

---
+++ src/components/TasksForm.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/taskSlice";
+import { v4 as uuid } from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
+
+const TasksForm = () => {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+  const tasks = useSelector((state) => state.tasks);
+
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (params.id) {
+      dispatch(editTask(task));
+    } else {
+      dispatch(
+        addTask({
+          ...task,
+          id: uuid(),
+        })
+      );
+    }
+
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
+    }
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        onChange={handleChange}
+        name="title"
+        value={task.title}
+      />
+      <textarea
+        name="description"
+        placeholder="Description"
+        onChange={handleChange}
+        value={task.description}></textarea>
+
+      <button>Save</button>
+    </form>
+  );
+};
+
+export default TasksForm;
